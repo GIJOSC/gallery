f790ac24765dbd148a85061837678f6c4a17c0ca
gijosc <neves.gesiel@gmail.com>
2023-01-13 11:12:10 -0300

    feat: Upload photos to firebase

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import { Photo } from "./types/Photo";
 import { PhotoItem } from "./components/PhotoItem";
 
 const App = () => {
+  const [uploading, setUploading] = useState(false);
   const [loading, setLoading] = useState(false);
   const [photos, setPhotos] = useState<Photo[]>([]);
 
@@ -17,11 +18,25 @@ const App = () => {
     getPhotos();
   }, []);
 
-  const handleFormSubmit = (e: FormEvent<HTMLFormElement>) => {
+  const handleFormSubmit = async (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
 
     const formData = new FormData(e.currentTarget);
     const file = formData.get("image") as File;
+
+    if (file && file.size > 0) {
+      setUploading(true);
+      let result = await Photos.insert(file);
+      setUploading(false);
+
+      if (result instanceof Error) {
+        alert(`${result.name} - ${result.message}`);
+      } else {
+        let newPhotoList = [...photos];
+        newPhotoList.push(result);
+        setPhotos(newPhotoList);
+      }
+    }
   };
 
   return (
@@ -32,6 +47,7 @@ const App = () => {
         <C.UploadForm method="POST" onSubmit={handleFormSubmit}>
           <input type="file" name="image" />
           <input type="submit" value="Enviar" />
+          {uploading && "Enviando..."}
         </C.UploadForm>
 
         {loading && (
